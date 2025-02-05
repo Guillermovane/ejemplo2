cb0b61e9027570e5950fa4fce56b6db9c41634b2
Guillermovane <guillermovb.ti22@utsjr.edu.mx>
2025-02-05 11:28:50 -0600

    nuevo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DadoComponent } from "./dado/dado.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DadoComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'] // Corregido
 })
 export class AppComponent {
   title = 'ejemplo2';
+
+  // Propiedades
+  resultado: string = "";
+  valorDado1: number = this.generarAleatorio();
+  valorDado2: number = this.generarAleatorio();
+  valorDado3: number = this.generarAleatorio();
+
+  // Función que genera un número aleatorio entre 1 y 6
+  generarAleatorio() {
+    return Math.floor(Math.random() * 6) + 1;
+  }
+
+  // Función que hace la tirada y verifica el resultado
+  tirarDados() {
+    this.valorDado1 = this.generarAleatorio();
+    this.valorDado2 = this.generarAleatorio();
+    this.valorDado3 = this.generarAleatorio();
+
+    // Comparamos si los 3 son iguales
+    if (this.valorDado1 === this.valorDado2 && this.valorDado1 === this.valorDado3) {
+      this.resultado = "Felicidades, ¡GANASTE!";
+    } else {
+      this.resultado = "¡Perdiste!";
+    }
+  }
 }
